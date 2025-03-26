25ce9ccb166f22205c40a9e6cc4bca6655995f6a
vaishu1205 <jsaivaishnavi15@gmail.com>
2025-03-26 14:15:56 +0530

    product_repository

---
+++ lib/repositories/product_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/product_model.dart';
+
+class ProductRepository {
+  final String apiUrl = 'https://dummyjson.com/products';
+
+  // Fetch products with pagination
+  Future<List<Product>> fetchProducts({int limit = 10, int skip = 0}) async {
+    try {
+      final response = await http.get(Uri.parse('$apiUrl?limit=$limit&skip=$skip'));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+
+        if (data.containsKey('products')) {
+          List<dynamic> productsJson = data['products'];
+
+          // Convert JSON to Product model list
+          return productsJson.map((json) => Product.fromJson(json)).toList();
+        } else {
+          throw Exception('Invalid API response format');
+        }
+      } else {
+        throw Exception('Failed to load products (Status: ${response.statusCode})');
+      }
+    } catch (e) {
+      print('Error fetching products: $e');
+      throw Exception('Error loading products');
+    }
+  }
+}
